c2eac5383c4d371286e9c28ff61bc673ab230b51
Anton <andrushin.anton@gmail.com>
2016-05-20 12:01:47 +0200

    Added avatars

--- app/serializers/api/v1/user_serializer.rb
+++ app/serializers/api/v1/user_serializer.rb
@@ -1,9 +1,13 @@
 class Api::V1::UserSerializer < ActiveModel::Serializer
-  attributes :id, :email, :first_name, :last_name, :rating, :about, :birth_date, :gender, :phone, :web_site, :country, :city, :created_at, :sports
+  attributes :id, :email, :first_name, :last_name, :rating, :about, :birth_date, :gender, :phone, :web_site, :country, :city, :created_at, :avatar, :sports
 
   def sports
   	object.sports.map do |sport|
       Api::V1::SportSerializer.new(sport, scope: scope, root: false, event: object)
     end
+  end
+
+  def avatar
+  	object.avatar.url.sub! 's3.amazonaws.com/windfollow', 'windfollow.s3.amazonaws.com'
   end
 end
