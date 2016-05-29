448a0c74d6043b27fdf637925eed049cb27ea27e
Anton <andrushin.anton@gmail.com>
2016-05-29 23:44:41 +0200

    updated report likes

--- app/serializers/api/v1/report_serializer.rb
+++ app/serializers/api/v1/report_serializer.rb
@@ -1,7 +1,11 @@
 class Api::V1::ReportSerializer < ActiveModel::Serializer
-  attributes :id, :spot_id, :content, :user_id, :place, :wind, :direction, :comments
+  attributes :id, :spot_id, :content, :user_id, :place, :wind, :direction, :comments, :likes
 
   def comments
   	object.report_comments.count(:all)
+  end
+
+  def likes
+  	object.report_likes.count(:all)
   end
 end
