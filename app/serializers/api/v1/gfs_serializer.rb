d07af8b19cc52b0fd0cec81a1fa1c99ec149a407
Anton <andrushin.anton@gmail.com>
2016-08-23 21:32:03 +0200

    Added APCP param to GFS model serializer

--- app/serializers/api/v1/gfs_serializer.rb
+++ app/serializers/api/v1/gfs_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::GfsSerializer < ActiveModel::Serializer
-  attributes :rt, :vt, :lat, :lon, :wind_dir, :wind_dir_deg, :wind_speed, :temp, :GUST_0, :CPRAT_0, :UGRD_1, :VGRD_1, :TCDC_925, :TCDC_700, :TCDC_500
+  attributes :rt, :vt, :lat, :lon, :wind_dir, :wind_dir_deg, :wind_speed, :temp, :GUST_0, :CPRAT_0, :UGRD_1, :VGRD_1, :TCDC_925, :TCDC_700, :TCDC_500, :APCP_0
 
   def temp
     # T(°C) = T(K) - 273.15
