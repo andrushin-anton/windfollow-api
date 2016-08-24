006cd5ad17f561be45d652d0c52f418855a5dd3d
Anton <andrushin.anton@gmail.com>
2016-08-24 19:22:20 +0200

    Added prate param

--- app/serializers/api/v1/gfs_serializer.rb
+++ app/serializers/api/v1/gfs_serializer.rb
@@ -1,5 +1,9 @@
 class Api::V1::GfsSerializer < ActiveModel::Serializer
-  attributes :rt, :vt, :lat, :lon, :wind_dir, :wind_dir_deg, :wind_speed, :temp, :GUST_0, :CPRAT_0, :UGRD_1, :VGRD_1, :TCDC_925, :TCDC_700, :TCDC_500, :APCP_0
+  attributes :rt, :vt, :lat, :lon, :wind_dir, :wind_dir_deg, :wind_speed, :temp, :GUST_0, :CPRAT_0, :UGRD_1, :VGRD_1, :TCDC_925, :TCDC_700, :TCDC_500, :APCP_0, :prate
+
+  def prate
+    return object.CSNOW_0
+  end
 
   def temp
     # T(°C) = T(K) - 273.15
