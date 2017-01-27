752b3bb5094fe267658939f73f574dc3164187a2
Anton <andrushin.anton@gmail.com>
2017-01-27 14:22:44 +0200

    fix with auth

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -3,8 +3,8 @@ class Api::V1::AuthController < ApplicationController
 	# POST /api/v1/auth/token
   # POST /api/v1/auth/token.json
   def token
-  	@user = Api::V1::User.new(api_v1_user_params)
-  	if @user.validate && @user = @user.authorize(params[:email], params[:password])
+    @user = Api::V1::User.where('email = ?', params[:email]).first
+  	if @user = @user.authorize(params[:email], params[:password])
   		# Create a new token
   		render json: @user.generate_token
   	elsif @user.nil?
