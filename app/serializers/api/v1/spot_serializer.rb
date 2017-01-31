8e4305d334eac0e5d1c5ef3364409e0ccb8f51b9
Anton <andrushin.anton@gmail.com>
2017-01-31 14:11:19 +0200

    should fix error when getting spots

--- app/serializers/api/v1/spot_serializer.rb
+++ app/serializers/api/v1/spot_serializer.rb
@@ -19,7 +19,9 @@ class Api::V1::SpotSerializer < ActiveModel::Serializer
   	months = {'1' => 0.0 ,'2' => 0.0 ,'3' => 0.0 ,'4' => 0.0 ,'5' => 0.0 ,'6' => 0.0 ,'7' => 0.0 ,'8' => 0.0 ,'9' => 0.0 ,'10' => 0.0 ,'11' => 0.0 ,'12' => 0.0}
   	estimates = Api::V1::SpotEstimate.where('spot_id = ?', object.id).all
 
-  	object.best_month.split(',').map { |k| months[k] = months[k] + 1 }
+    unless object.best_month.nil?
+      object.best_month.split(',').map { |k| months[k] = months[k] + 1 }
+    end
 
   	if estimates
   		estimates.each do |item|
