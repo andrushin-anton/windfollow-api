7812dc5988eb7a4dd2aabb3918a90af0172792ca
Anton <andrushin.anton@gmail.com>
2017-06-01 22:11:59 +0200

    Added conversation_id to message serializer

--- app/serializers/api/v1/message_serializer.rb
+++ app/serializers/api/v1/message_serializer.rb
@@ -1,8 +1,12 @@
 class Api::V1::MessageSerializer < ActiveModel::Serializer
-  attributes :body, :sender, :created_at, :read, :updated_at
+  attributes :body, :sender, :conversation_id, :created_at, :read, :updated_at
 
   def sender
   	Api::V1::UserFollowerSerializer.new(object.user, { root: false })
+  end
+
+  def conversation_id
+    object.conversation.id
   end
 
 end
