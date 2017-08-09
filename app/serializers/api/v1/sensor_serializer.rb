ec7a0b8a8e511116b6356f9d7a3cd8ea1b0d5265
Anton <andrushin.anton@gmail.com>
2017-08-10 01:34:46 +0200

    Added more data in sensors_data response

--- app/serializers/api/v1/sensor_serializer.rb
+++ app/serializers/api/v1/sensor_serializer.rb
@@ -2,6 +2,6 @@ class Api::V1::SensorSerializer < ActiveModel::Serializer
   attributes :name, :slug, :geo_lat, :geo_lon, :data
 
   def data
-    Api::V1::SensorData.where('sensor_id = ?', object.id).order('created_at DESC').first
+    Api::V1::SensorData.where('sensor_id = ? AND created_at >= NOW() - INTERVAL 1 HOUR', object.id).order('created_at DESC').all
   end
 end
