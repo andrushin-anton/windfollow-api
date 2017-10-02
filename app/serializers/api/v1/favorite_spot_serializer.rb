7424794b7c8451874fc76fdd11ca4ca538082f90
Anton <andrushin.anton@gmail.com>
2017-10-02 22:49:54 +0200

    changed favorite spots serializer

--- app/serializers/api/v1/favorite_spot_serializer.rb
+++ app/serializers/api/v1/favorite_spot_serializer.rb
@@ -1,7 +1,7 @@
 class Api::V1::FavoriteSpotSerializer < ActiveModel::Serializer
   attributes :id, :user_id, :spot, :closed
-
+  
   def spot
-  	Api::V1::Spot.where('id = ?', object.spot_id).first  	
+  	Api::V1::SpotSerializer.new(Api::V1::Spot.where('id = ?', object.spot_id).first, { root: false })
   end
 end
