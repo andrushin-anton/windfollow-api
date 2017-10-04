a89f912ab939ddeb1d7dda73d7e4cb4dc60be909
Anton <andrushin.anton@gmail.com>
2017-10-05 00:57:20 +0200

    updated capistrano with whenever gem

--- app/models/api/v1/sensor_data.rb
+++ app/models/api/v1/sensor_data.rb
@@ -2,6 +2,6 @@ class Api::V1::SensorData < ActiveRecord::Base
     belongs_to :sensor
 
     def clean_up
-        Api::V1::SensorData.where(:created_at <= (Time.current - 1.day).to_s(:db)).delete_all
+        Api::V1::SensorData.where('created_at <= ?', (Time.current - 1.day).to_s(:db)).delete_all
     end
 end
