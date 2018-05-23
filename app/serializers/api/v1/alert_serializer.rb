1a65ef83b5e28b18fccac58509e6b35d903b4e3b
Anton Andrushin <andrushin.anton@gmail.com>
2018-05-24 00:08:21 +0200

    add notify_for_days param

--- app/serializers/api/v1/alert_serializer.rb
+++ app/serializers/api/v1/alert_serializer.rb
@@ -1,5 +1,5 @@
 class Api::V1::AlertSerializer < ActiveModel::Serializer
-  attributes :id, :user_id, :spot, :direction, :time_alert, :speed_from, :speed_to
+  attributes :id, :user_id, :spot, :direction, :time_alert, :speed_from, :speed_to, :notify_for_days
 
   def spot
   	Api::V1::SpotSerializer.new(object.spot, { root: false })
